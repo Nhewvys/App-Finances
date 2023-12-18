b306ac77ec9c445c0c6ed2424b134ef24a0fa0e9
Nhewvys Batista <nhewvys.silva@gmail.com>
2023-12-18 16:47:21 -0300

    Refatorando

--- main.js
+++ main.js
@@ -1,67 +1,59 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const sectionCode = urlParams.get('section-code');
+const urlParams = new URLSearchParams(window.location.search);
+const sectionCode = urlParams.get('section-code');
 
-    const sections = document.querySelectorAll('section');
+const sections = document.querySelectorAll('section');
+const allSideMenu = document.querySelectorAll('.sidebar ul .sidemenu-main li');
+const menu = document.querySelector('.fa-solid.fa-bars');
+const sidebar = document.querySelector('.sidebar');
 
-    if (sectionCode) {
-        sections.forEach(section => {
-            if (section.id === sectionCode) {
-                section.style.display = 'block';
-            } else {
-                section.style.display = 'none';
-            }
-        });
-
-        const allSideMenu = document.querySelectorAll('.sidebar ul .sidemenu-main li');
-        allSideMenu.forEach(item => {
-            if (item.querySelector('i').getAttribute('id') === sectionCode) {
-                allSideMenu.forEach(i => {
-                    i.classList.remove('active');
-                });
-                item.classList.add('active');
-            }
-        });
-    } else {
-        // Se não houver nenhum 'section-code' na URL, exibe a primeira seção por padrão
-        sections[0].style.display = 'block';
-        // Adicione a classe 'active' ao primeiro item do menu por padrão
-        const allSideMenu = document.querySelectorAll('.sidebar ul .sidemenu-main li');
-        allSideMenu[0].classList.add('active');
-    }
-
-    const menu = document.querySelector('.fa-solid.fa-bars');
-    const sidebar = document.querySelector('.sidebar');
-
-    menu.addEventListener('click', () => {
-        sidebar.classList.toggle('hide');
+function showSection(sectionId) {
+    sections.forEach(section => {
+        section.style.display = section.id === sectionId ? 'block' : 'none';
     });
+}
 
-    const allSideMenu = document.querySelectorAll('.sidebar ul .sidemenu-main li');
+function setActiveMenuItem(sectionId) {
     allSideMenu.forEach(item => {
-        item.addEventListener('click', function(event) {
-            event.preventDefault(); // Evita o comportamento padrão do link
-
-            const sectionId = item.querySelector('i').getAttribute('id');
-            const queryParams = new URLSearchParams(window.location.search);
-            queryParams.set('section-code', sectionId);
-
-            const newUrl = `${window.location.pathname}?${queryParams.toString()}`;
-            history.pushState({}, '', newUrl);
-
-            // Adiciona a classe 'active' ao item do menu clicado
-            allSideMenu.forEach(i => {
-                i.classList.remove('active');
-            });
-            item.classList.add('active');
-
-            sections.forEach(section => {
-                if (section.id === sectionId) {
-                    section.style.display = 'block';
-                } else {
-                    section.style.display = 'none';
-                }
-            });
-        });
+        const itemId = item.querySelector('i').getAttribute('id');
+        item.classList.toggle('active', itemId === sectionId);
     });
-});
+}
+
+function updateUrlAndState(sectionId) {
+    const queryParams = new URLSearchParams(window.location.search);
+    queryParams.set('section-code', sectionId);
+
+    const newUrl = `${window.location.pathname}?${queryParams.toString()}`;
+    history.pushState({}, '', newUrl);
+}
+
+function handleSideMenuItemClick(event) {
+    event.preventDefault();
+
+    const sectionId = this.querySelector('i').getAttribute('id');
+    updateUrlAndState(sectionId);
+
+    allSideMenu.forEach(i => i.classList.remove('active'));
+    this.classList.add('active');
+
+    showSection(sectionId);
+}
+
+function handleMenuClick() {
+    sidebar.classList.toggle('hide');
+}
+
+// Show default section or section from URL
+if (sectionCode) {
+    showSection(sectionCode);
+    setActiveMenuItem(sectionCode);
+} else {
+    // Show the first section by default
+    showSection(sections[0].id);
+    allSideMenu[0].classList.add('active');
+}
+
+// Event listeners
+menu.addEventListener('click', handleMenuClick);
+allSideMenu.forEach(item => item.addEventListener('click', handleSideMenuItemClick));
+
